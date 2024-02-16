0036da0f70172ee12f5bc761352ac2e11e6d1fce
Navaiklam <dmlk.dark@gmail.com>
2024-02-16 04:10:25 -0300

    Se agrego colores y algunos detalles más

--- telereversos.sh
+++ telereversos.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+rojo='\033[0;31m'
+verde='\033[0;32m'
+azul='\033[0;34m'
+reset='\033[0m'
+
 archivo="archivo.txt"
 archivo_nuevo="archivo_nuevo.txt"
 
@@ -34,11 +39,10 @@ procesar_archivo() {
     # Crear un nuevo archivo con el contenido del archivo temporal
     if [ -s "$archivo_temporal" ]; then
         cp "$archivo_temporal" "$archivo_nuevo"
-        echo "Se creó un nuevo archivo con las IPs únicas: $archivo_nuevo"
+        echo -e "${azul}\nSe creó un nuevo archivo con las IPs únicas: $archivo_nuevo${reset}\n"
     else
-        echo "No se encontraron IPs únicas en el archivo."
+        echo -e "${rojo}No se encontraron IPs únicas en el archivo${reset}"
     fi
-
 
     rm "$archivo_temporal"
 }
@@ -50,18 +54,19 @@ while IFS= read -r linea; do
     for ip in $ips; do
         
         if iptables-save | grep -q "$ip"; then
-            echo "La IP $ip ya está agregada en iptables"
+            echo -e "${rojo}La IP $ip ya está agregada en iptables${reset}"
         else
         #iptables -A OUTPUT -s 192.168.88.226 -d $ip.0/24 -j DROP
             iptables -A OUTPUT -s 192.168.88.218 -d $ip.0/24 -j DROP
-            echo "Se agrego el siguiente rango $ip.0/24"
+            echo -e "${verde}Se agrego el siguiente rango${reset} ${azul}$ip.0/24${reset}"
             
             
         fi
     done
 done < "$archivo_nuevo"
-echo -e "Así quedo la tabla OUTPUT de iptables \n"
+echo -e "\n ${azul}Así quedo la tabla OUTPUT de iptables${reset}"
 iptables -L OUTPUT --line-numbers
+echo -e "\n"
 }
 
 
@@ -71,25 +76,25 @@ procesar_archivo "$archivo"
 #verifica si el archivo existe y llama a la función
 if [ -f "$archivo_nuevo" ]; then
     add_iptables "$archivo_nuevo"
-    while true; do
-        echo "Por favor, ingresa una opción (s, n) para guardar los cambios en iptables:"
-        echo "Ten en cuanta que se guarda en /etc/iptables/iptables.rules"
-        read opcion  
-        case "$opcion" in
-            "s")
-                iptables-save > /etc/iptables/iptables.rules
-                echo "Se guardo correctamente en /etc/iptables/iptables.rules"
-                break 
-                ;;
-            "n")
-                echo "No se guardo ningún cambio"
-                break  
-                ;;
-            *)
-                echo "Opción no válida. Por favor, ingresa una opción válida."
-                ;;
-        esac
-    done
+        while true; do
+            echo -e "${azul}Por favor, ingresa una opción${reset} (${rojo}S${reset}, ${verde}N${reset}) ${azul}para guardar los cambios en iptables:${reset}"
+            echo -e "${rojo}Ten en cuanta que se guarda en /etc/iptables/iptables.rules${reset}"
+            read opcion  
+            case "$opcion" in
+                "S")
+                    iptables-save > /etc/iptables/iptables.rules
+                    echo "Se guardo correctamente en /etc/iptables/iptables.rules"
+                    break 
+                    ;;
+                "N")
+                    echo "No se guardo ningún cambio"
+                    break  
+                    ;;
+                *)
+                    echo "Opción no válida. Por favor, ingresa una opción válida."
+                    ;;
+            esac
+        done
 else
-    echo "Nada que agregar a iptable SI/NO"
+    echo "Nada que agregar a iptable"
 fi
